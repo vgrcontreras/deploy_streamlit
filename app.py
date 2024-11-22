5b1f8e5b0542cfd22aea293cde4f63f910c4de3d
Victor Contreras <contreras3991@gmail.com>
2024-11-22 15:40:01 -0300

    alterado

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-ano = st.slider(
-    label='Escolha um ano',
-    min_value=1993,
-    max_value=2024
-)
+# ano = st.slider(
+#     label='Escolha um ano',
+#     min_value=1993,
+#     max_value=2024
+# )
 
-st.write(f'Você escolheu o ano {ano}')
+# st.write(f'Você escolheu o ano {ano}')
+
+st.image('./julia.png')
